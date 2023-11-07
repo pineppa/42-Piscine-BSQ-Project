f27871c706595f013bff5a6ff881eb62b0248084
Jacopo Sala <jsala@cbr6s3.42barcelona.com>
2023-11-07 12:35:47 +0100

    Fixed ft_print_sol.c to use type t_coord. Corrected norminette issues

--- srcs/ft_print_sol.c
+++ srcs/ft_print_sol.c
@@ -6,31 +6,33 @@
 /*   By: jsala <jsala@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 11:08:48 by jsala             #+#    #+#             */
-/*   Updated: 2023/11/07 12:16:01 by jsala            ###   ########.fr       */
+/*   Updated: 2023/11/07 12:34:55 by jsala            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft.h"
 
-void    ft_print_sol(int **cached, char *symbols, t_coord sizes, t_coord pos_max)
+void	ft_print_sol(int **cached, char *sym, t_coord sizes, t_coord pos_max)
 {
-	t_coord p;
-	int i;
+	t_coord	p;
+	int		i;
+	int		val;
 
 	p.r = -1;
 	p.c = -1;
 	i = 0;
+	val = cached[pos_max.r][pos_max.c];
 	while (++p.r < sizes.r)
 	{
 		while (++p.c < sizes.c)
 		{
 			if (cached[p.r][p.c] == 0)
-				ft_putchar(symbols[1]);
-			else if ((pos_max.r < p.r && p.r < pos.r + val) 
-					&& (pos_max.c < sizes.c && p.c < pos.c + val))
-				ft_putchar(symbols[2]);
+				ft_putchar(sym[1]);
+			else if ((pos_max.r < p.r && p.r < pos_max.r + val)
+				&& (pos_max.c < sizes.c && p.c < pos_max.c + val))
+				ft_putchar(sym[2]);
 			else
-				ft_putchar(symbols[0]);
+				ft_putchar(sym[0]);
 		}
 		write(1, "\n", 1);
 	}
